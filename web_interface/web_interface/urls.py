e803066829e170966593ecd9c7cf2e9961086557
5675856 <alex.kudrin.01@gmail.com>
2024-06-10 19:57:01 +0300

    new file:   web_interface/startweb.py 	modified:   web_interface/web_interface/urls.py

--- web_interface/web_interface/urls.py
+++ web_interface/web_interface/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('devices.urls')),
+    path('charts/', include('charts.urls')),
+    path('authorization/', include('authorization.urls')),
+    path('setpoints/', include('setpoints.urls')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
