9a319eb85194d0ae2c57f70e7dd81f9c11969429
5675856 <alex.kudrin.01@gmail.com>
2024-06-12 21:29:00 +0300

    modified:   web_interface/authorization/models.py 	modified:   web_interface/charts/models.py 	modified:   web_interface/devices/models.py

--- web_interface/authorization/models.py
+++ web_interface/authorization/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Usertab(models.Model):
+    user_id=models.IntegerField()
+    client_id=models.IntegerField()
+    name=models.CharField()
+    login=models.CharField()
+    password=models.CharField()
+    tg_id=models.CharField()
+
+    class Meta:
+        managed=False
+        db_table='USER'
+
+class Clienttab(models.Model):
+    client_id=models.IntegerField()
+    name=models.CharField()
+    rule_id=models.IntegerField()
+
+    class Meta:
+        managed=False
+        db_table='CLIENT'
+
+class Rulestab(models.Model):
+    rule_id=models.IntegerField()
+    web=models.BooleanField()
+    setting=models.BooleanField()
+    control=models.BooleanField()
+    report=models.BooleanField()
+    bot=models.BooleanField()
+
+    class Meta:
+        managed=False
+        db_table='RULES'
--- web_interface/charts/models.py
+++ web_interface/charts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class LogTimetab(models.Model):
+    log_id=models.IntegerField()
+    device_id=models.IntegerField()
+    timestamp_loc=models.DateTimeField()
+    timestamp_dev=models.DateTimeField()
+    GMT=models.CharField()
+    status=models.IntegerField()
+    depth=models.IntegerField()
+    power=models.IntegerField()
+    status_string=models.IntegerField()
+    data=models.CharField()
+
+    class Meta:
+        managed=False
+        db_table='LOG_TIME'
+
+class LogEventtab(LogTimetab):
+
+    class Meta:
+        managed=False
+        db_table='LOG_EVENT'
