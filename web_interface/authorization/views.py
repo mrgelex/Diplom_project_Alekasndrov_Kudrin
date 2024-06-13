bc8642ac0dd8bd07ee1970ab24c012334886f189
5675856 <alex.kudrin.01@gmail.com>
2024-06-13 17:36:42 +0300

    new file:   web_interface/authorization/forms.py 	new file:   web_interface/authorization/templates/authorization/basic.html 	new file:   web_interface/authorization/templates/authorization/log-in-out.html 	modified:   web_interface/authorization/views.py

--- web_interface/authorization/views.py
+++ web_interface/authorization/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from .forms import Loginform
 
-# Create your views here.
+def showLogin(request):
+    if request.method == 'POST':
+        logindata=Loginform(request.POST)
+        if logindata.is_valid():
+            pass
+        else:
+            return HttpResponse('Неправильный ввод данных!')
