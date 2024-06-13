5e041f1f5d367ddbba36e71e8843ba5526653fc7
5675856 <alex.kudrin.01@gmail.com>
2024-06-13 20:01:09 +0300

    modified:   web_interface/authorization/views.py

--- web_interface/authorization/views.py
+++ web_interface/authorization/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
-from .forms import Loginform
+from .forms import *
 
 def showLogin(request):
     if request.method == 'POST':
-        logindata=Loginform(request.POST)
-        if logindata.is_valid():
-            pass
+        dataform=Loginform(request.POST)
+        if dataform.is_valid():
+            lgn=dataform.cleaned_data.get('login')
+            pswd=dataform.cleaned_data.get('password')
+            validUser=Usertab.objects.filter(login=lgn, password=pswd)
+            if validUser:
+                pass
+            else:
+                return HttpResponse('Неправильный логин или пароль!')
         else:
-            return HttpResponse('Неправильный ввод данных!')
+            return HttpResponse('Неправильный ввод!')
+    form=Loginform()
+    return render(request, 'authorization/login', {'form':form})
