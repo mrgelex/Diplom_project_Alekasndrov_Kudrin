19997db20565009b4c66a860c8722e5187305d4b
5675856 <alex.kudrin.01@gmail.com>
2024-07-09 17:08:32 +0300

    modified:   web_interface/devices/views.py

--- web_interface/devices/views.py
+++ web_interface/devices/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.urls import reverse
 from .models import *
 
-webAcces=0
+webAcces=10
 
 class Bush:
-    def __init__(self, folder_id, name, rule_id, deviceSet):
+    def __init__(self, folder_id, name, rule_id, deviceSet=None):
         self.id=folder_id
         self.name=name
         self.rule=rule_id
@@ -15,13 +14,12 @@ class Bush:
         if self.rule > ruleCli:
             self.rule=ruleCli
 
-class Device:
-    def __init__(self,device_id, name_user):
-        self.device_id=device_id
-        self.name_user=name_user
-
 
 def allFold(request):
+
+    def addnestFold():
+        pass
+
     if not 'user' in request.session:
         print('нет пользователя')
         return redirect('login')
@@ -40,30 +38,45 @@ def allFold(request):
     allFold=Foldertab.objects.all()
     if not 'rootFold' in request.session:
         print('нет корневых')
-        userPerm=UserPermtab.objects.filter(user_id=user.get('userid'))
-        if len(userPerm) < 2:
-            print('userPerm < 2')
-            foldListOne=list(userPerm.values('folder_id', 'rule_id'))
-            foldDataOne=allFold.get(folder_id=foldListOne[0].get('folder_id'))
-            foldListOne[0]['name']=foldDataOne.name
-            foldListOne[0]['root_folder']=foldDataOne.root_folder
-            request.session['oneFold']=foldListOne
+        userPerm=UserPermtab.objects.filter(user_id=user.get('userid'))# это все права пользователя в querySet
+        foldList=list(userPerm.values('folder_id', 'rule_id')) # это лист со словорями права для каталогов
+        foldListMany=[]
+        if len(foldList) < 2: # 1 каталог?
+            print('foldList < 2')
+            oneFoldId=foldList[0].get('folder_id')# id этого каталога
+            foldDataOne=allFold.get(folder_id=oneFoldId) # выбрать данные по одному каталогу
+            foldList[0]['name']=foldDataOne.name # добавить имя каталога к правам
+            foldList[0]['root_folder']=foldDataOne.root_folder # добавить id корневого каталога к правам
+            if foldDataOne.root_folder: # этот каталог не корневой?
+                print('один каталог группа с устройствами')
+                request.session['oneFold']=foldList
+                return redirect('onlyBush')
+                # return redirect(reverse('showbush', args=[foldDataOne.root_folder]))
+            nestFold=list(allFold.filter(root_folder=oneFoldId).values('folder_id', 'root_folder', 'name')) # каталог 1 и он корневой; найти вложенные каталоги в лист
+            for i in nestFold:
+                i['rule_id']=foldList[0].get('rule_id') # каждой группе добавиить роль корневого каталога
+                foldListMany.append(i)
+            request.session['manyFold']=foldListMany # пользователь имеет доступ ко всем группа в одном корневом
             return redirect('onlyBush')
             # return redirect(reverse('showbush', args=[foldDataOne.root_folder]))
-        foldDictList=list(userPerm.values('folder_id', 'rule_id'))
-        foldListMany=[]
-        for i in foldDictList:
-            foldDataMany=allFold.get(folder_id=i.get('folder_id'))
-            i['name']=foldDataMany.name
-            i['root_folder']=foldDataMany.root_folder
-            foldListMany.append(i)
-        request.session['manyFold']=foldListMany
+            
+        # for i in foldList:
+        #     foldDataMany=allFold.get(folder_id=i.get('folder_id'))
+        #     i['name']=foldDataMany.name
+        #     i['root_folder']=foldDataMany.root_folder
+        #     foldListMany.append(i)
+        # request.session['manyFold']=foldListMany # добавить в сессию каталоги (с данными) доступные пользователю
         folIdList=[]
         for i in userPerm:
-            folIdList.append(i.folder_id)
+            folIdList.append(i.folder_id) # собрать все id
         rootFolList=[]
         for i in folIdList:
-            rootFolList=rootFolList+list(allFold.filter(folder_id=i).values_list('root_folder', flat=True))
+            listFol=list(allFold.filter(folder_id=i).values_list('root_folder', flat=True)) # выбрать все корневые по id
+            if not listFol[0]: # это корневой
+                pass #найти вложенные в лист, присвоить роль, записать с сессию, записать каталог root
+                rootFolList=rootFolList+listFol
+            else:
+                pass
         rootFolList=list(set(rootFolList))
         if len(rootFolList) < 2:
             print('меньше двух корневых')
@@ -93,10 +106,10 @@ def showBush(request, idFol):
     selectFold=[]
     for i in foldData:
         if i.get('root_folder')==idFol:
-            devices=Devicetab.objects.filter(folder_id=i.get('folder_id'))
-            bushObj=Bush(i.get('folder_id'), i.get('name'), i.get('rule_id'), devices)
+            bushObj=Bush(i.get('folder_id'), i.get('name'), i.get('rule_id'))
             bushObj.access(user.get('clrule'))
-            if bushObj.rule > webAcces:
+            if bushObj.rule >= webAcces:
+                bushObj.deviceSet=Devicetab.objects.filter(folder_id=i.get('folder_id'))
                 selectFold.append(bushObj)
     if not selectFold:
         return render(request, 'devices/warning.html', {'text':'Извините, Ваш уровень просмотра ограничен'})
@@ -112,11 +125,11 @@ def onlyBush(request):
 
     selectFold=[]
     for i in foldData:
-        devices=Devicetab.objects.filter(folder_id=i.get('folder_id'))
-        bushObj=Bush(i.get('folder_id'), i.get('name'), i.get('rule_id'), devices)
-        bushObj.access(user.get('clrule'))
-        if bushObj.rule > webAcces:
-            selectFold.append(bushObj)
+        bushObj=Bush(i.get('folder_id'), i.get('name'), i.get('rule_id'))#1
+        bushObj.access(user.get('clrule'))#2
+        devices=Devicetab.objects.filter(folder_id=i.get('folder_id'))#4 
+        if bushObj.rule >= webAcces:#3
+            selectFold.append(bushObj)#5
     if not selectFold:
         return render(request, 'devices/warning.html', {'text':'Извините, Ваш уровень просмотра ограничен'})
     return render(request, 'devices/bushes.html', {'selectFold':selectFold,'user':user})
